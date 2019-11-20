f87de20ed824f5eb947ecfb823e8ac89af1a4ac9
bastienwcs <email>
2019-11-20 17:27:28 +0100

    fix

--- src/main/java/com/wildcodeschool/harrypotter/controller/HarryPotterController.java
+++ src/main/java/com/wildcodeschool/harrypotter/controller/HarryPotterController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
